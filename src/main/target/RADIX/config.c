2ff15212fee4da3675f85f3eccc73114bdea03c4
Bruce Luckcuck <github@etracer.net>
2020-02-26 13:19:20 -0500

    BrainFPV Radix minimal target

---
+++ src/main/target/RADIX/config.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <platform.h>
+
+#ifdef USE_TARGET_CONFIG
+
+#include "config/config.h"
+
+#include "flight/pid.h"
+#include "flight/mixer.h"
+#include "sensors/gyro.h"
+#include "blackbox/blackbox.h"
+#include "osd/osd.h"
+#include "pg/motor.h"
+
+void targetConfiguration(void)
+{
+    pidConfigMutable()->pid_process_denom = 1;
+    blackboxConfigMutable()->p_ratio = 128;
+    motorConfigMutable()->dev.motorPwmProtocol = PWM_TYPE_DSHOT600;
+    osdConfigMutable()->rssi_alarm = 70; // for CRSF
+}
+#endif
